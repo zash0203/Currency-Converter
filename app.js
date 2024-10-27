88ef4d26af7a82c4b8cc32b26d2b5396f238fa4e
Zeeshan Abdullah Kha <65828265+zash0203@users.noreply.github.com>
2024-10-27 00:06:22 -0400

    Update app.js
    Created New API

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies"
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
@@ -32,10 +31,17 @@ const updateExchangeRate = async () => {
     amtVal = 1;
     amount.value = "1";
   }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.min.json`;
   let response = await fetch(URL);
   let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
+  console.log(data);
+  let from = fromCurr.value.toLowerCase();
+  console.log(from);
+  let to = toCurr.value.toLowerCase();
+  console.log(to);
+
+  let rate = data[from][to];
+
 
   let finalAmount = amtVal * rate;
   msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
@@ -49,11 +55,14 @@ const updateFlag = (element) => {
   img.src = newSrc;
 };
 
-btn.addEventListener("click", (evt) => {
+btn.addEventListener("click",(evt) => {
   evt.preventDefault();
+
   updateExchangeRate();
 });
 
-window.addEventListener("load", () => {
+window.addEventListener("load",() => {
   updateExchangeRate();
 });
+
+
